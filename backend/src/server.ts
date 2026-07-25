a2a47d3490beaa2cea9dab2fe9fea185f0a7cc1e
natnael <natibeyene49@gmail.com>
2026-07-25 16:32:31 +0300

    add neccessary env codes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import { PORT } from "./config/env";
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
