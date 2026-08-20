e628dc95c6fd8b43f352aea4b3318897fcd12a00
natnael <natibeyene49@gmail.com>
2026-08-20 20:12:22 +0300

    fix login issues

--- backend/src/config/couchdb.ts
+++ backend/src/config/couchdb.ts
@@ -1,34 +1,18 @@
 import Nano from "nano";
 
 import {
+  COUCHDB_DATABASE,
+  COUCHDB_PASSWORD,
   COUCHDB_URL,
   COUCHDB_USERNAME,
-  COUCHDB_PASSWORD,
-  COUCHDB_DATABASE,
 } from "./env.js";
-
-if (!COUCHDB_URL) {
-  throw new Error("COUCHDB_URL is missing from environment variables.");
-}
-
-if (!COUCHDB_USERNAME) {
-  throw new Error("COUCHDB_USERNAME is missing from environment variables.");
-}
-
-if (!COUCHDB_PASSWORD) {
-  throw new Error("COUCHDB_PASSWORD is missing from environment variables.");
-}
-
-if (!COUCHDB_DATABASE) {
-  throw new Error("COUCHDB_DATABASE is missing from environment variables.");
-}
 
 export const couch = Nano(COUCHDB_URL);
 
 export const databaseName = COUCHDB_DATABASE;
 
+export const db = couch.use(databaseName);
+
 export async function authenticateCouchDb() {
   await couch.auth(COUCHDB_USERNAME, COUCHDB_PASSWORD);
 }
-
-export const db = couch.use(databaseName);
